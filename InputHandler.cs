577638f18e868dbe112a66ac2efc8a1df7807662
GodWasaProgrammer <bwnils@gmail.com>
2022-12-11 20:32:32 +0100

    Made an Input Method, that handles parsing and possible fallout.

--- InputHandler.cs
+++ InputHandler.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CostOfTile
 {
-    internal class InputHandler
+    public class InputHandler
     {
+        int Width;
+        int Length;
+        int CostPerTile;
+        static void FloorCalculations(int Width, int Length, int CostPerTile)
+        {
+            // Here we should calculate the cost of the flooring process 
+            // something like
+            // 
+            int FloorSize = Width * Length;
+            int TotalCost = FloorSize * CostPerTile;
+            Console.WriteLine($"Your total cost is:{TotalCost}");
+        }
+         void Input()
+        {
+            bool IsParsable; // Ensures that we successfully parsed
+            do
+            {
+                Console.WriteLine("Input the Width of the floor");
+                IsParsable = Int32.TryParse(Console.ReadLine(), out Width);
+            }
+            while (IsParsable);
+            IsParsable= false;
+            do
+            { 
+                Console.WriteLine("Insert the length of your floor");
+                IsParsable = Int32.TryParse(Console.ReadLine(), out Length);
+            }
+            while (IsParsable);
+            IsParsable= false;
+            do
+            {
+                Console.WriteLine("Insert the cost of your Tile");
+                IsParsable = Int32.TryParse (Console.ReadLine(), out CostPerTile);
+            }
+            while (IsParsable);
+            IsParsable= false;
+        }
     }
 }
