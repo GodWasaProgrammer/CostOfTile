915713c17cb929724557c7dd9ad338eb6a670318
GodWasaProgrammer <bwnils@gmail.com>
2022-12-11 19:49:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CostOfTile
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Tile Cost Processing software");
+            Console.WriteLine("Here you will enter width and length of floor");
+            Console.WriteLine("And also the cost per floor tile");
+            Console.WriteLine("This software will then return the cost of the total");
+
+        }
+    }
+}
