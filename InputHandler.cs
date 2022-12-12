baa74cd6bc7dc8ccf7b8124eef5719142faf638a
GodWasaProgrammer <bwnils@gmail.com>
2022-12-12 22:43:55 +0100

    Cleaned up blanks and redundant commentaries

--- InputHandler.cs
+++ InputHandler.cs
@@ -10,16 +10,15 @@ namespace CostOfTile
         int Width;
         int Length;
         int CostPerTile;
-        int AreaToCover;
-        void FloorCalculations(int Width, int Length, int CostPerTile)
+        
+        static void FloorCalculations(int Width, int Length, int CostPerTile)
         {
             // Here we should calculate the cost of the flooring process 
             // something like
             // 
             int FloorSize = Width * Length;
             int TotalCost = FloorSize * CostPerTile;
-            int result = TotalCost * AreaToCover;
-            Console.WriteLine($"Your total cost is:{result}");
+            Console.WriteLine($"Your total cost is:{TotalCost}");
         }
          public void Input()
         {
@@ -42,13 +41,6 @@ namespace CostOfTile
             {
                 Console.WriteLine("Insert the cost of your Tile");
                 IsParsable = Int32.TryParse (Console.ReadLine(), out CostPerTile);
-            }
-            while (IsParsable == false);
-            IsParsable = false;
-            do
-            {
-                Console.WriteLine("Insert the room area you wish to cover");
-                IsParsable = Int32.TryParse(Console.ReadLine(), out AreaToCover);
             }
             while (IsParsable == false);
             IsParsable = false;
