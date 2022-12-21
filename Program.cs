21de66d7ede82d00e3b11060ade27f1468f436b7
GodWasaProgrammer <bwnils@gmail.com>
2022-12-21 04:22:14 +0100

    1. Remove unnecessary overload - done 2. move all to main - done 3 remove class, functions - done 4. make procedural - done 5. input check re-added 3 loops and error message if wrong - done 6. made all doubles instead of ints - done 7. That was BS. made them decimals instead. - done 8. relearn pythagoras 9 square feet... americans.. good night

--- Program.cs
+++ Program.cs
@@ -4,12 +4,58 @@
     {
         static void Main()
         {
+            decimal width;
+            decimal length;
+            decimal costPerTile;
+            bool widthIsParsable;
+            bool lengthIsParsable;
+            bool costPerTileIsParsable;
+            decimal floorSize;
+            decimal totalCost;
             Console.WriteLine("Welcome to the Tile Cost Processing software");
             Console.WriteLine("Here you will enter width and length of floor");
             Console.WriteLine("And also the cost per floor tile");
             Console.WriteLine("This software will then return the cost of the total");
-            var costPerTileCalculator = new CostPerTileCalculator();
-            costPerTileCalculator.Input();
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Input the Width of the floor");
+                    widthIsParsable = decimal.TryParse(Console.ReadLine(), out width);
+                    if (widthIsParsable == false)
+                    {
+                        Console.WriteLine("Your input wasnt a number. Try again.");
+                    }
+                } while (widthIsParsable == false);
+
+                do
+                {
+                    Console.WriteLine("Insert the length of your floor");
+                    lengthIsParsable = decimal.TryParse(Console.ReadLine(), out length);
+                    if (lengthIsParsable == false)
+                    {
+                    }
+                } while (lengthIsParsable == false);
+
+                do
+                {
+                    Console.WriteLine("Insert the cost of your Tile");
+                    costPerTileIsParsable = decimal.TryParse(Console.ReadLine(), out costPerTile);
+                    if (costPerTileIsParsable == false)
+                    {
+                        Console.WriteLine("Your input wasnt a number. Try again.");
+                    }
+                } while (costPerTileIsParsable == false);
+
+            }
+            while (widthIsParsable == false || lengthIsParsable == false || costPerTileIsParsable == false);
+
+            // Here we should calculate the cost of the flooring process 
+            // something like
+            // 
+            floorSize = width * length;
+            totalCost = floorSize * costPerTile;
+            Console.WriteLine($"Your total cost is:{totalCost}");
         }
     }
 }
