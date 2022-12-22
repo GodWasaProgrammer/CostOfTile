6b5d2366d18a7b1c9b2829fa48fca8f712bf9d22
GodWasaProgrammer <bwnils@gmail.com>
2022-12-22 04:59:14 +0100

    went back to using 1 bool for the parsing, and set it to false after each loop is successful.
    Added some fuckery who might calculate the area of a triangular floor, or perhaps the equation of whats in a black hole. who knows, not me.

--- Program.cs
+++ Program.cs
@@ -7,55 +7,74 @@
             decimal width;
             decimal length;
             decimal costPerTile;
-            bool widthIsParsable;
-            bool lengthIsParsable;
-            bool costPerTileIsParsable;
             decimal floorSize;
-            decimal totalCost;
+            decimal totalCost = 0;
+            bool isParsable;
             Console.WriteLine("Welcome to the Tile Cost Processing software");
             Console.WriteLine("Here you will enter width and length of floor");
             Console.WriteLine("And also the cost per floor tile");
             Console.WriteLine("This software will then return the cost of the total");
+
             do
             {
-                do
+                Console.WriteLine("Input the Width of the floor");
+                isParsable = decimal.TryParse(Console.ReadLine(), out width);
+                if (isParsable == false)
                 {
-                    Console.WriteLine("Input the Width of the floor");
-                    widthIsParsable = decimal.TryParse(Console.ReadLine(), out width);
-                    if (widthIsParsable == false)
-                    {
-                        Console.WriteLine("Your input wasnt a number. Try again.");
-                    }
-                } while (widthIsParsable == false);
+                    Console.WriteLine("Your input wasnt a number. Try again.");
+                }
 
-                do
+            } while (isParsable == false);
+
+            isParsable = false;
+            do
+            {
+                Console.WriteLine("Insert the length of your floor");
+                isParsable = decimal.TryParse(Console.ReadLine(), out length);
+                if (isParsable == false)
                 {
-                    Console.WriteLine("Insert the length of your floor");
-                    lengthIsParsable = decimal.TryParse(Console.ReadLine(), out length);
-                    if (lengthIsParsable == false)
-                    {
-                    }
-                } while (lengthIsParsable == false);
+                    Console.WriteLine("Your input wasnt a number. Try again.");
+                }
 
-                do
+            } while (isParsable == false);
+
+            isParsable = false;
+            do
+            {
+                Console.WriteLine("Insert the cost of your Tile");
+                isParsable = decimal.TryParse(Console.ReadLine(), out costPerTile);
+                if (isParsable == false)
                 {
-                    Console.WriteLine("Insert the cost of your Tile");
-                    costPerTileIsParsable = decimal.TryParse(Console.ReadLine(), out costPerTile);
-                    if (costPerTileIsParsable == false)
-                    {
-                        Console.WriteLine("Your input wasnt a number. Try again.");
-                    }
-                } while (costPerTileIsParsable == false);
+                    Console.WriteLine("Your input wasnt a number. Try again.");
+                }
 
-            }
-            while (widthIsParsable == false || lengthIsParsable == false || costPerTileIsParsable == false);
+            } while (isParsable == false);
 
-            // Here we should calculate the cost of the flooring process 
+
+            Console.WriteLine("Is your floor a Rectangle? if yes, type y, otherwise, type n");
+            string ?floorType = Console.ReadLine();
+
+            // Here we should calculate the cost of the flooring process on a rectangular room
             // something like
             // 
-            floorSize = width * length;
-            totalCost = floorSize * costPerTile;
+            if (floorType == "y")
+            {
+                floorSize = width * length;
+                totalCost = floorSize * costPerTile;
+                
+            }
+            
+            // and if its not a rectangle its a god damn triangle right? lets F'n hope so.
+            if (floorType == "n")
+            {
+                floorSize = width * length / 2;
+                totalCost = floorSize * costPerTile;
+            }
+
             Console.WriteLine($"Your total cost is:{totalCost}");
+
+            
+
         }
     }
 }
